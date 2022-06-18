22f68f73706286bd398394f6701ee9435d471ada
lobob291 <s3896743@student.rmit.edu.au>
2022-06-18 15:12:58 +1000

    updated pages for subtask 3.2

--- src/main/java/app/LGAST32.java
+++ src/main/java/app/LGAST32.java
@@ -33,22 +33,49 @@ public class LGAST32 {
    public int get2016Homeless() {
        return homeless2016;
    }
-   public int getChangeHomeless() {
+
+   public int get2018Homeless() {
+      return homeless2018;
+  }
+
+   public int getatRisk2016() {
+   return atRisk2016;
+  }
+
+   public int getatRisk2018() {
+   return atRisk2018;
+  }
+
+   public int gettotalPopulation2016() {
+   return totalPopulation2016;
+  }
+
+   public int gettotalPopulation2018() {
+   return totalPopulation2018;
+  }
+
+   public int getChangeHomeless(int homeless2018, int homeless2016) {
+      changeHomeless = homeless2018 - homeless2016;
     return changeHomeless;
  }
- public int getChangeAtRisk() {
+ public int getChangeAtRisk(int atRisk2018, int atRisk2016) {
+      changeAtRisk = atRisk2018 - atRisk2016;
     return changeAtRisk;
  }
- public int getChangeTotalPopulation() {
+ public int getChangeTotalPopulation(int totalPopulation2018, int totalPopulation2016) {
+      changeTotalPopulation = totalPopulation2018 - totalPopulation2016;
     return changeTotalPopulation;
  }
- public double getChangeHomelessPercent() {
+ public double getChangeHomelessPercent(int homeless2018, int homeless2016) {
+      changeHomelessPercent = ((homeless2018 - homeless2016)/(homeless2016)) * 100;
     return changeHomelessPercent;
  }
- public double getChangeAtRiskPercent() {
+ public double getChangeAtRiskPercent(int atRisk2018, int atRisk2016) {
+      changeAtRiskPercent = ((atRisk2018 - atRisk2016)/(atRisk2016)) * 100;
     return changeAtRiskPercent;
  }
- public double getChangeTotalPopulationPercent() {
+ public double getChangeTotalPopulationPercent(int totalPopulation2018, int totalPopulation2016) {
+      changeTotalPopulationPercent = ((totalPopulation2018 - totalPopulation2016)/(totalPopulation2016)) * 100;
     return changeTotalPopulationPercent;
  }
 }
